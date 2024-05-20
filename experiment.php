e9310f550573c84480fa152238b099e0383461ff
Jorge Duarte <sadujorge@gmail.com>
2024-05-20 15:48:03 -0700

    Added sample theme starter code

--- experiment.php
+++ experiment.php
@@ -6,9 +6,18 @@
     <title>Simple PHP Page</title>
 </head>
 <body>
-    <h1>Welcome to My Website</h1>
+    <!-- $ will allow us to make a varible -->
+    <?php $place = 'Website' ?>
+    <?php function myFirstFunction() {echo 2 + 2;}
+        myFirstFunction();
+    ?>
+    <h1>Welcome to My <?php echo $place ?></h1>
     <p>This is a simple HTML page with PHP!</p>
+    <!-- Echo outputs executed code to page -->
     <p>Today's date is: <?php echo date('Y-m-d'); ?>.</p>
+    <p> <?php echo myFirstFunction() ?></p>
+
+    <!-- <h2><?php bloginfo('name'); ?></h2> -->
 </body>
 </html>
 
