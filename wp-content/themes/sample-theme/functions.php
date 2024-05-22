f931bf624afb5e7b428c009d29103d99d7120500
Jorge Duarte <sadujorge@gmail.com>
2024-05-22 10:44:29 -0700

    Setup initial css

--- wp-content/themes/sample-theme/functions.php
+++ wp-content/themes/sample-theme/functions.php
@@ -1,7 +1,8 @@
 <?php
 
 function enqueue_main_styles() {
-    wp_enqueue_style( 'main_styles', get_stylesheet_uri());
+    wp_enqueue_style( 'main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style( 'extra_styles', get_theme_file_uri('/build/index.css'));
 }
 
 add_action( 'wp_enqueue_scripts', 'enqueue_main_styles' );
