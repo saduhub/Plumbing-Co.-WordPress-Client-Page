321f2172ac4fc6343cc598cf18ef725fa015ea32
Jorge Duarte <sadujorge@gmail.com>
2024-05-22 10:46:42 -0700

    Setup font awesome icons

--- wp-content/themes/sample-theme/functions.php
+++ wp-content/themes/sample-theme/functions.php
@@ -1,6 +1,7 @@
 <?php
 
 function enqueue_main_styles() {
+    wp_enqueue_style( 'font_awesome_icons', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style( 'main_styles', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style( 'extra_styles', get_theme_file_uri('/build/index.css'));
 }
