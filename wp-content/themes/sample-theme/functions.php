a8472a9a5054eeb68dbf2b6571929e2aa70105e0
Jorge Duarte <sadujorge@gmail.com>
2024-05-22 10:50:32 -0700

    Setup google fonts and imported main website code

--- wp-content/themes/sample-theme/functions.php
+++ wp-content/themes/sample-theme/functions.php
@@ -1,6 +1,7 @@
 <?php
 
 function enqueue_main_styles() {
+    wp_enqueue_style( 'google_fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style( 'font_awesome_icons', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style( 'main_styles', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style( 'extra_styles', get_theme_file_uri('/build/index.css'));
