29f9f309bd3d7f88c50dffc879b216c9face1b63
Jorge Duarte <sadujorge@gmail.com>
2024-05-23 16:15:07 -0700

    Created custom post type for future posts pertaining to events instead of blog posts. Custom post type creation code will be stored in new folder labeled mu-plugins so that theme always has access to custom port type created

--- wp-content/themes/sample-theme/functions.php
+++ wp-content/themes/sample-theme/functions.php
@@ -1,14 +1,14 @@
 <?php
 
 function enqueue_main_styles() {
-    wp_enqueue_script( 'main_js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
-    wp_enqueue_style( 'google_fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-    wp_enqueue_style( 'font_awesome_icons', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style( 'main_styles', get_theme_file_uri('/build/style-index.css'));
-    wp_enqueue_style( 'extra_styles', get_theme_file_uri('/build/index.css'));
+    wp_enqueue_script('main_js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+    wp_enqueue_style('google_fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font_awesome_icons', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('extra_styles', get_theme_file_uri('/build/index.css'));
 }
 
-add_action( 'wp_enqueue_scripts', 'enqueue_main_styles' );
+add_action('wp_enqueue_scripts', 'enqueue_main_styles');
 
 function theme_features() {
     // Register menus that can be modified from admin portal.
@@ -18,5 +18,20 @@ function theme_features() {
     // Add title tag to each tab.
     add_theme_support('title-tag');
 }
+// After setup theme, call theme_features function.
+add_action('after_setup_theme', 'theme_features');
 
-add_action( 'after_setup_theme', 'theme_features' );
+function custom_post_types() {
+    register_post_type (
+        'event',
+        array(
+            'public' => true,
+            'labels' => array(
+                'name' => 'Custom Post Type'
+            ),
+            'menu_icon' => 'dashicons-admin-generic'
+        )
+    );
+}
+
+add_action('init', 'custom_post_types');
