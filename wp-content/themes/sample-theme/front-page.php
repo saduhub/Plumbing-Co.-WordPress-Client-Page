37aac70a8f1847e17d9606cf699d32d286b7363c
Jorge Duarte <sadujorge@gmail.com>
2024-05-23 15:28:25 -0700

    Navigation to blog page (hard coded and on the wp admin panel)

--- wp-content/themes/sample-theme/front-page.php
+++ wp-content/themes/sample-theme/front-page.php
@@ -69,8 +69,8 @@
                 wp_reset_postdata();
 
             ?>
-
-          <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
+          <!-- WP functionadded to the root domain to redirect to desired url-->
+          <p class="t-center no-margin"><a href="<?php echo site_url('/blog')?>" class="btn btn--yellow">View All Blog Posts</a></p>
         </div>
       </div>
     </div>
