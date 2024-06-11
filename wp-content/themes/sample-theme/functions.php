82e34020f5bc2c758f4d3fbbbd3187c75e4db031
Jorge Duarte <sadujorge@gmail.com>
2024-06-10 17:33:36 -0700

    Removed unecessary custom query and adjusted code so all locations are shown on the map on load

--- wp-content/themes/sample-theme/functions.php
+++ wp-content/themes/sample-theme/functions.php
@@ -62,6 +62,10 @@ add_action('after_setup_theme', 'theme_features');
 
 // Manipulate default URL based queries.
 function adjust_events_query($query) {
+    if (!is_admin() AND is_post_type_archive('campus') AND $query-> is_main_query()) { 
+        $query->set('posts_per_page', -1);  
+    }
+
     if (!is_admin() AND is_post_type_archive('program') AND $query-> is_main_query()) {
         $query->set('orderby', 'title');
         $query->set('order', 'ASC'); 
