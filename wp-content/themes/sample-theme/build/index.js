e8879230cb73ccb03f516b2a84bfd73911b2b218
Jorge Duarte <sadujorge@gmail.com>
2024-06-11 17:07:26 -0700

    Added request for pages as well as posts and set them up to run asynchronously

--- wp-content/themes/sample-theme/build/index.js
+++ wp-content/themes/sample-theme/build/index.js
@@ -234,14 +234,17 @@ class Search {
   }
   getResults() {
     // Arrow function employs to avoive chining bind method. This is meant to point to the results div and not the .getJSON method result.
-    jquery__WEBPACK_IMPORTED_MODULE_0___default().getJSON(universityData.root_url + "/wp-json/wp/v2/posts?search=" + this.searchField.val(), posts => {
+    jquery__WEBPACK_IMPORTED_MODULE_0___default().when(jquery__WEBPACK_IMPORTED_MODULE_0___default().getJSON(universityData.root_url + "/wp-json/wp/v2/posts?search=" + this.searchField.val()), jquery__WEBPACK_IMPORTED_MODULE_0___default().getJSON(universityData.root_url + "/wp-json/wp/v2/pages?search=" + this.searchField.val())).then((posts, pages) => {
+      var combinedResults = posts[0].concat(pages[0]);
       this.resultsDiv.html(`
-        <h2 class="search-overlay__section-title">Search Results</h2>
-        ${posts.length ? '<ul class="link-list min-list">' : "<p>No Items Match This Search.</p>"}
-          ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join("")}
-        ${posts.length ? "</ul>" : ""}
+        <h2 class="search-overlay__section-title">General Information</h2>
+        ${combinedResults.length ? '<ul class="link-list min-list">' : "<p>No general information matches that search.</p>"}
+          ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join("")}
+        ${combinedResults.length ? "</ul>" : ""}
       `);
       this.isSpinnerVisible = false;
+    }, () => {
+      this.resultsDiv.html("<p>Unexpected error; please try again.</p>");
     });
   }
   keyPressDispatcher(e) {
--- wp-content/themes/sample-theme/src/modules/Search.js
+++ wp-content/themes/sample-theme/src/modules/Search.js
@@ -40,15 +40,21 @@ class Search {
   }
   getResults() {
     // Arrow function employs to avoive chining bind method. This is meant to point to the results div and not the .getJSON method result.
-    $.getJSON(universityData.root_url + "/wp-json/wp/v2/posts?search=" + this.searchField.val(), posts => {
-      this.resultsDiv.html(`
-        <h2 class="search-overlay__section-title">Search Results</h2>
-        ${posts.length ? '<ul class="link-list min-list">' : "<p>No Items Match This Search.</p>"}
-          ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join("")}
-        ${posts.length ? "</ul>" : ""}
+    $.when($.getJSON(universityData.root_url + "/wp-json/wp/v2/posts?search=" + this.searchField.val()), $.getJSON(universityData.root_url + "/wp-json/wp/v2/pages?search=" + this.searchField.val())).then(
+      (posts, pages) => {
+        var combinedResults = posts[0].concat(pages[0])
+        this.resultsDiv.html(`
+        <h2 class="search-overlay__section-title">General Information</h2>
+        ${combinedResults.length ? '<ul class="link-list min-list">' : "<p>No general information matches that search.</p>"}
+          ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join("")}
+        ${combinedResults.length ? "</ul>" : ""}
       `)
-      this.isSpinnerVisible = false
-    })
+        this.isSpinnerVisible = false
+      },
+      () => {
+        this.resultsDiv.html("<p>Unexpected error; please try again.</p>")
+      }
+    )
   }
   keyPressDispatcher(e) {
     if (e.keyCode == 83 && !this.isOverlayOpen && !$("input, textarea").is(":focus")) {
