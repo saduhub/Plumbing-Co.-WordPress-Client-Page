e8a5c707acc9d23852c1fc77bcf9aa549a01b733
Jorge Duarte <sadujorge@gmail.com>
2024-06-11 17:00:21 -0700

    Added input reset when ser exits and enters search overlay.

--- wp-content/themes/sample-theme/src/modules/Search.js
+++ wp-content/themes/sample-theme/src/modules/Search.js
@@ -62,6 +62,7 @@ class Search {
   openOverlay() {
     this.searchOverlay.addClass("search-overlay--active");
     $("body").addClass("body-no-scroll");
+    this.searchField.val('');
     setTimeout(() => this.searchField.focus(), 301);
     this.isOverlayOpen = true;
   }
