6ced1961a72fffb9394ddd0a1dce1dd0719c3b03
Jorge Duarte <sadujorge@gmail.com>
2024-06-13 15:44:46 -0700

    Added code for customized login and sign up forms.

--- wp-content/themes/sample-theme/functions.php
+++ wp-content/themes/sample-theme/functions.php
@@ -136,3 +136,25 @@ function noSubsAdminBar() {
     show_admin_bar(false);
   }
 }
+
+// Customize Login Screen
+add_filter('login_headerurl', 'ourHeaderUrl');
+
+function ourHeaderUrl() {
+  return esc_url(site_url('/'));
+}
+
+add_action('login_enqueue_scripts', 'ourLoginCSS');
+
+function ourLoginCSS() {
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+  wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+}
+
+add_filter('login_headertitle', 'ourLoginTitle');
+
+function ourLoginTitle() {
+  return get_bloginfo('name');
+}
