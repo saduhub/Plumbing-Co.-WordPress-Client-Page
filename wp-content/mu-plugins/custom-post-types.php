75cb0a569bac912c6ecf14ee547a2752a6c746c6
Jorge Duarte <sadujorge@gmail.com>
2024-06-13 16:14:03 -0700

    Registered notes port type (Admin + mu-plugins) and added code to query the notes created and display the corresponding HTML.

--- wp-content/mu-plugins/custom-post-types.php
+++ wp-content/mu-plugins/custom-post-types.php
@@ -87,6 +87,21 @@ function custom_post_types() {
             'menu_icon' => 'dashicons-admin-generic'
         )
     );
+
+    register_post_type('note', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor'),
+        'public' => false,
+        'show_ui' => true,
+        'labels' => array(
+          'name' => 'Notes',
+          'add_new_item' => 'Add New Note',
+          'edit_item' => 'Edit Note',
+          'all_items' => 'All Notes',
+          'singular_name' => 'Note'
+        ),
+        'menu_icon' => 'dashicons-admin-generic'
+    ));
 }
 
 add_action('init', 'custom_post_types');
