1d3618e01241d9f55f527faa41929a665d9229a9
Jorge Duarte <sadujorge@gmail.com>
2024-06-13 17:06:38 -0700

    Added code to specifically delete the note clicked on. Removed hardcoded value.

--- wp-content/themes/sample-theme/build/index.js
+++ wp-content/themes/sample-theme/build/index.js
@@ -205,15 +205,23 @@ class MyNotes {
   }
   events() {
     jquery__WEBPACK_IMPORTED_MODULE_0___default()(".delete-note").on("click", this.deleteNote);
+    jquery__WEBPACK_IMPORTED_MODULE_0___default()(".edit-note").on("click", this.editNote);
   }
-  deleteNote() {
+  editNote(e) {
+    var thisNote = jquery__WEBPACK_IMPORTED_MODULE_0___default()(e.target).parents("li");
+    thisNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass("note-active-field");
+    thisNote.find(".update-note").addClass("update-note--visible");
+  }
+  deleteNote(e) {
+    var thisNote = jquery__WEBPACK_IMPORTED_MODULE_0___default()(e.target).parents("li");
     jquery__WEBPACK_IMPORTED_MODULE_0___default().ajax({
       beforeSend: xhr => {
         xhr.setRequestHeader("X-WP-Nonce", universityData.nonce);
       },
-      url: universityData.root_url + "/wp-json/wp/v2/note/128",
+      url: universityData.root_url + "/wp-json/wp/v2/note/" + thisNote.data("id"),
       type: "DELETE",
       success: response => {
+        thisNote.slideUp();
         console.log("Congrats");
         console.log(response);
       },
