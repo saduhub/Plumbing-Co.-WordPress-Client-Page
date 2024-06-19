08b5c70cee2f9cf0a3877efd32d82a59b453e0ba
Jorge Duarte <sadujorge@gmail.com>
2024-06-19 12:38:24 -0700

    Added logic to display plugin settings page on admin panel. Created Class to improve function naming compatibility with WP hooks. Functions will trigger to create settings page once a new instance of the class is initiated.

--- wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
+++ wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
@@ -1,30 +1,27 @@
 <?php
 
-  /*
-    Plugin Name: Our Test Plugin
-    Description: A truly amazing plugin.
-    Version: 1.0
-    Author: Brad
-    Author URI: https://www.udemy.com/user/bradschiff/
-  */
+/*
+  Plugin Name: Our Test Plugin
+  Description: A truly amazing plugin.
+  Version: 1.0
+  Author: Brad
+  Author URI: https://www.udemy.com/user/bradschiff/
+*/
 
-  class WordCountAndTimePlugin {
-    function __construct() {
-      add_action('admin_menu', array($this, 'adminPage'));
-    }
-
-    function adminPage() {
-      add_options_page('Word Count Settings', 'Word Count', 'manage_options', 'word-count-settings-page', array($this, 'ourHTML'));
-    }
-    
-    function ourHTML() { 
-?>
-
-<div class="wrap">
-  <h1>Word Count Settings</h1>
-</div>
-
-<?php }
+class WordCountAndTimePlugin {
+  function __construct() {
+    add_action('admin_menu', array($this, 'adminPage'));
   }
-  $wordCountAndTimePlugin = new WordCountAndTimePlugin();
-?>
+
+  function adminPage() {
+    add_options_page('Word Count Settings', 'Word Count', 'manage_options', 'word-count-settings-page', array($this, 'ourHTML'));
+  }
+
+  function ourHTML() { ?>
+    <div class="wrap">
+      <h1>Word Count Settings</h1>
+    </div>
+  <?php }
+}
+
+$wordCountAndTimePlugin = new WordCountAndTimePlugin();
