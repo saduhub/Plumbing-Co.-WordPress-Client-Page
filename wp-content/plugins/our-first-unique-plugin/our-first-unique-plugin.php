132eb0a084baf9ca329421437b1cb0ced22769b2
Jorge Duarte <sadujorge@gmail.com>
2024-06-19 13:31:53 -0700

    Added translations plugin and used admin dashboard to create the desired language files. Added sanitization to entries in another language.(esc_html__ line 46)

--- wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
+++ wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
@@ -6,6 +6,8 @@
   Version: 1.0
   Author: Brad
   Author URI: https://www.udemy.com/user/bradschiff/
+  Text Domain: wcpdomain
+  Domain Path: /languages
 */
 
 class WordCountAndTimePlugin {
@@ -13,6 +15,11 @@ class WordCountAndTimePlugin {
     add_action('admin_menu', array($this, 'adminPage'));
     add_action('admin_init', array($this, 'settings'));
     add_filter('the_content', array($this, 'ifWrap'));
+    add_action('init', array($this, 'languages'));
+  }
+
+  function languages() {
+    load_plugin_textdomain('wcpdomain', false, dirname(plugin_basename(__FILE__)) . '/languages');
   }
 
   function ifWrap($content) {
@@ -36,7 +43,7 @@ class WordCountAndTimePlugin {
     }
 
     if (get_option('wcp_wordcount', '1')) {
-      $html .= 'This post has ' . $wordCount . ' words.<br>';
+      $html .= esc_html__('This post has', 'wcpdomain') . ' ' . $wordCount . ' ' . __('words', 'wcpdomain') . '.<br>';
     }
 
     if (get_option('wcp_charactercount', '1')) {
@@ -99,7 +106,7 @@ class WordCountAndTimePlugin {
   <?php }
 
   function adminPage() {
-    add_options_page('Word Count Settings', 'Word Count', 'manage_options', 'word-count-settings-page', array($this, 'ourHTML'));
+    add_options_page('Word Count Settings', __('Word Count', 'wcpdomain'), 'manage_options', 'word-count-settings-page', array($this, 'ourHTML'));
   }
 
   function ourHTML() { ?>
