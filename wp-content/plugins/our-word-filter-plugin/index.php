8e05da9c0ffb4c79cf67b8567f37a8032c694442
Jorge Duarte <sadujorge@gmail.com>
2024-06-19 14:24:31 -0700

    Added function to replace text specified. Finished settings form for Options inside Word Filter. Added necessary code to save replacement word into database.

--- wp-content/plugins/our-word-filter-plugin/index.php
+++ wp-content/plugins/our-word-filter-plugin/index.php
@@ -13,6 +13,25 @@ if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 class OurWordFilterPlugin {
   function __construct() {
     add_action('admin_menu', array($this, 'ourMenu'));
+    add_action('admin_init', array($this, 'ourSettings'));
+    if (get_option('plugin_words_to_filter')) add_filter('the_content', array($this, 'filterLogic'));
+  }
+
+  function ourSettings() {
+    add_settings_section('replacement-text-section', null, null, 'word-filter-options');
+    register_setting('replacementFields', 'replacementText');
+    add_settings_field('replacement-text', 'Filtered Text', array($this, 'replacementFieldHTML'), 'word-filter-options', 'replacement-text-section');
+  }
+
+  function replacementFieldHTML() { ?>
+    <input type="text" name="replacementText" value="<?php echo esc_attr(get_option('replacementText', '***')) ?>">
+    <p class="description">Leave blank to simply remove the filtered words.</p>
+  <?php }
+
+  function filterLogic($content) {
+    $badWords = explode(',', get_option('plugin_words_to_filter'));
+    $badWordsTrimmed = array_map('trim', $badWords);
+    return str_ireplace($badWordsTrimmed, esc_html(get_option('replacementText', '****')), $content);
   }
 
   function ourMenu() {
@@ -42,7 +61,7 @@ class OurWordFilterPlugin {
   function wordFilterPage() { ?>
     <div class="wrap">
       <h1>Word Filter</h1>
-      <?php if (isset($_POST['justsubmitted']) == "true") $this->handleForm() ?>
+      <?php if ($_POST['justsubmitted'] == "true") $this->handleForm() ?>
       <form method="POST">
         <input type="hidden" name="justsubmitted" value="true">
         <?php wp_nonce_field('saveFilterWords', 'ourNonce') ?>
@@ -56,7 +75,17 @@ class OurWordFilterPlugin {
   <?php }
 
   function optionsSubPage() { ?>
-    Hello world from the options page.
+    <div class="wrap">
+      <h1>Word Filter Options</h1>
+      <form action="options.php" method="POST">
+        <?php
+          settings_errors();
+          settings_fields('replacementFields');
+          do_settings_sections('word-filter-options');
+          submit_button();
+        ?>
+      </form>
+    </div>
   <?php }
 
 }
