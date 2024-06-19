6e3935dec5d7d72eadc11cb07eb8f1bbc636d5d8
Jorge Duarte <sadujorge@gmail.com>
2024-06-19 12:48:33 -0700

    Added settings function to register the settings to be added to the setting page and to the database (display location). Within the function, the HTML and location was assigned to the settings field being registered.

--- wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
+++ wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
@@ -11,7 +11,21 @@
 class WordCountAndTimePlugin {
   function __construct() {
     add_action('admin_menu', array($this, 'adminPage'));
+    add_action('admin_init', array($this, 'settings'));
   }
+
+  function settings() {
+    add_settings_section('wcp_first_section', null, null, 'word-count-settings-page');
+    add_settings_field('wcp_location', 'Display Location', array($this, 'locationHTML'), 'word-count-settings-page', 'wcp_first_section');
+    register_setting('wordcountplugin', 'wcp_location', array('sanitize_callback' => 'sanitize_text_field', 'default' => '0'));
+  }
+
+  function locationHTML() { ?>
+    <select name="wcp_location">
+      <option value="0">Beginning of post</option>
+      <option value="1">End of post</option>
+    </select>
+  <?php }
 
   function adminPage() {
     add_options_page('Word Count Settings', 'Word Count', 'manage_options', 'word-count-settings-page', array($this, 'ourHTML'));
@@ -20,6 +34,13 @@ class WordCountAndTimePlugin {
   function ourHTML() { ?>
     <div class="wrap">
       <h1>Word Count Settings</h1>
+      <form action="options.php" method="POST">
+      <?php
+        settings_fields('wordcountplugin');
+        do_settings_sections('word-count-settings-page');
+        submit_button();
+      ?>
+      </form>
     </div>
   <?php }
 }
